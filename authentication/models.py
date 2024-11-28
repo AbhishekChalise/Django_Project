117169d1c74ca6fbdbb7aacb008846adda07fda2
AbhishekChalise <107126896+AbhishekChalise@users.noreply.github.com>
2024-11-28 12:19:23 +0545

    Commit 1 to the Crud operations

--- authentication/models.py
+++ authentication/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class AuthenticationModel(models.Model):
+
+    User = models.OneToOneField(User , on_delete = models.CASCADE) 
     is_teacher = models.BooleanField(default=False)
     is_student = models.BooleanField(default = False)
-    
     class Meta:
         db_table = 'authentication'
--- student/models.py
+++ student/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-
+from authentication.models  import AuthenticationModel
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Student(models.Model):
-    sid = models.CharField(max_length = 255)
+    #sid = models.CharField(max_length = 255)
     sname = models.CharField(max_length = 100)
     semail = models.EmailField(max_length = 100)
     scontact = models.CharField(max_length = 50)
     sdelete = models.BooleanField(default=False)
     class meta:
         db_table = "student"
+
+@receiver(post_save , sender  = AuthenticationModel)
+def create_Student(sender,instance,created,**kwargs):
+    if created and instance.is_student:
+        Student.objects.create(user = instance ,sname = instance.username)
+        print('Created Students for: ',instance.username)
+
+
     
 
 
